09a8859c89bb39fa485b6eb4fb99222944a2d98c
HusackJo <161376370+HusackJo@users.noreply.github.com>
2026-02-11 12:08:10 -0500

    Conveyor belt!

---
+++ Assets/Script/S_ConveyorBelt.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class S_ConveyorBelt : MonoBehaviour
+{
+    [Header("Do Physics Movement?")]
+    [SerializeField]
+    bool doPhysicsMovement;
+
+    [Header("Teleporting Settings")]
+    [SerializeField]
+    Transform pointToTeleportTo;
+
+    [Header("Physics movement settings")]
+    [SerializeField]
+    float beltSpeed;
+    [SerializeField]
+    float forceX = 10, forceZ = 0;
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Physics movement behaviors
+        if (!other.gameObject.CompareTag("Player") && other.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rB) && doPhysicsMovement)
+        {
+            Vector3 force = new Vector3(forceX, 0, forceZ);
+
+            rB.AddForce(force, ForceMode.Force);
+        }
+        //Teleporting behaviors
+        else if (!other.gameObject.CompareTag("Player") && !doPhysicsMovement)
+        {
+            other.transform.position = pointToTeleportTo.position;
+        }
+    }
+}
