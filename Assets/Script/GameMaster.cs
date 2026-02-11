ce236d71a0ed3d418fb15401f8dd83cf18c8d3f6
Fowlerpro <jaycefowler@outlook.com>
2026-02-11 12:08:35 -0500

    add 2d Sketchs add GameOver Framework add Timer add buttons in scene add levers add titlescreen scene

---
+++ Assets/Script/GameMaster.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    float endTime = 0;
+    public float startTime = 240;
+    bool timerActive = true;
+    public float countdownTimer = 5;
+    public GameObject scorePanel;
+    public GameObject namePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownTimer -= Time.deltaTime;
+        if (countdownTimer <= 0f)
+        {
+            timerActive = true;
+            Debug.Log("Game Start!");
+        }
+
+        if (timerActive)
+        {
+            startTime -= Time.deltaTime;
+            if (startTime <= 0f)
+            {
+                Debug.Log("Game Over!");
+                scorePanel.SetActive(true);
+                namePanel.SetActive(true);
+            }
+        }
+    }
+}
