90537fcece9dab805cc3881fe4f6b2a2a581bf48
anaha <anaharisohn@gmail.com>
2026-02-12 12:41:44 -0500

    Animtior Script Add and working.

--- Assets/CiyberGuy/Animtion/S_CG_Animtion.cs
+++ Assets/CiyberGuy/Animtion/S_CG_Animtion.cs
@@ -2,18 +2,17 @@ using UnityEngine;
 
 public class S_CG_Animtion : MonoBehaviour
 {
-    GameObject Mesh ;
+    Animator anim;
 
-    Vector3 g;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetFloat ("MoveSpeed", 1);
     }
 }
