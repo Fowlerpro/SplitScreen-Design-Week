56e3592c83d4e70e83cffb263c2d3705d8d9771a
anaha <anaharisohn@gmail.com>
2026-02-12 12:31:21 -0500

    hh

---
+++ Assets/CiyberGuy/Animtion/S_CG_Animtion.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class S_CG_Animtion : MonoBehaviour
+{
+    GameObject Mesh ;
+
+    Vector3 g;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
