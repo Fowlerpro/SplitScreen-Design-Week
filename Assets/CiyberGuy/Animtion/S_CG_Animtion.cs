6d6660528418f8499aadf2c31c657d7bd21e0fcc
anaha <anaharisohn@gmail.com>
2026-02-12 22:01:06 -0500

    Add Character Colors

--- Assets/CiyberGuy/Animtion/S_CG_Animtion.cs
+++ Assets/CiyberGuy/Animtion/S_CG_Animtion.cs
@@ -4,6 +4,17 @@ public class S_CG_Animtion : MonoBehaviour
 {
     public Animator anim;
 
+    public GameObject[] Colorables;
+
+    public GameObject[] PColorables;
+
+    public GameObject[] SColorables;
+
+    public Color[] PColors;
+    public Color[] SColors;
+
+    public int ColorIndex = 0;
+
     public void SetSpeed(float Speed)
     {
         anim.SetFloat("MoveSpeed", Speed);
@@ -17,6 +28,37 @@ public class S_CG_Animtion : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        ColorIndex = Random.Range(0, Colorables.Length);
+
+        for (int i = 0; i < Colorables.Length; i++)
+        {
+            var Rhair = Colorables[i].GetComponent<Renderer>();
+            if (Rhair != null)
+            {
+                Rhair.material.SetColor("_BaseColor", PColors[ColorIndex]);
+                Rhair.material.SetColor("_EmissionColor", SColors[ColorIndex]);
+            }
+        }
+
+        for (int i = 0; i < PColorables.Length; i++)
+        {
+            var Rhair = PColorables[i].GetComponent<Renderer>();
+            if (Rhair != null)
+            {
+                Rhair.material.SetColor("_EmissionColor", PColors[ColorIndex]);
+            }
+        }
+
+        for (int i = 0; i < SColorables.Length; i++)
+        {
+            var Rhair = SColorables[i].GetComponent<Renderer>();
+            if (Rhair != null)
+            {
+                Rhair.material.SetColor("_EmissionColor", SColors[ColorIndex]);
+            }
+        }
+
         anim = GetComponent<Animator>();
     }
 
